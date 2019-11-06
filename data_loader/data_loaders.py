c1eb31ce0d5f160d382c3686805ba8b668e2cf3c
yxr <suanrong@gmail.com>
2019-11-06 10:32:55 +0800

    ADD: class dataset

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,10 +1,22 @@
 from torchvision import datasets, transforms
+from torch.utils.data import Dataset
 from base import BaseDataLoader
 
+class xxDataset(Dataset):
+    def __init__(self, name):
+        self.name = name
+        self.data = None
+        self.len = None
+
+    def __len__(self):
+        return 0
+
+    def __getitem__(self, idx):
+        return self.data.features[idx], self.data.labels[idx]
 
 class MnistDataLoader(BaseDataLoader):
     """
-    MNIST data loading demo using BaseDataLoader
+    a sample MNIST data loading demo using BaseDataLoader
     """
     def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
         trsfm = transforms.Compose([
--- model/model.py
+++ model/model.py
@@ -4,6 +4,9 @@ from base import BaseModel
 
 
 class MnistModel(BaseModel):
+    """
+    a sample model
+    """
     def __init__(self, num_classes=10):
         super().__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
